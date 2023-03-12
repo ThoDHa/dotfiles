ba6d92a2b57cafccc7812470e3abfd59bbd72426
Tho Ha <tho.ha@gatech.edu>
2023-03-11 22:22:42 -0600

    Adding zshrc stuff

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # set up VIM TMUX and Bash
 mkdir ~/.vim/tmp/mkdir 
 
-sudo apt-get install tmux vim fzf fd-find exuberant-ctags
+sudo apt-get install tmux vim fzf fd-find bat exuberant-ctags
 curl https://raw.githubusercontent.com/ThoDHa/confs/master/vimrc > ~/.vimrc
 curl https://raw.githubusercontent.com/ThoDHa/confs/master/tmux.conf > ~/.tmux.conf
 curl https://raw.githubusercontent.com/ThoDHa/confs/master/ctags > ~/.ctags.d/default.ctags
@@ -21,7 +21,7 @@ git clone --depth 1 https://github.com/dense-analysis/ale.git ~/.vim/pack/git-pl
 sudo apt install ripgrep
 python3 -m pip install --user virtualenv
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 nvm install latest
 nvm install-latest-npm
 
@@ -43,66 +43,11 @@ if grep -q "source /usr/share/doc/fzf/examples/completion.bash" ~/.bashrc; then
 fi
 
 # zsh
-##options to fzf command
-#export FZF_COMPLETION_OPTS='--border --info=inline'
-
-## Use fd (https://github.com/sharkdp/fd) instead of the default find
-## command for listing path candidates.
-## - The first argument to the function ($1) is the base path to start traversal
-## - See the source code (completion.{bash,zsh}) for the details.
-#_fzf_compgen_path() {
-#	fd --hidden --follow --exclude ".git" . "$1"
-#}
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 #
-## Use fd to generate the list for directory completion
-#_fzf_compgen_dir() {
-#	fd --type d --hidden --follow --exclude ".git" . "$1"
-#}
-#
-## Advanced customization of fzf options via _fzf_comprun function
-## - The first argument to the function is the name of the command.
-## - You should make sure to pass the rest of the arguments to fzf.
-#_fzf_comprun() {
-#	local command=$1
-#		shift
-#
-#	case "$command" in
-#		cd)           fzf --preview 'tree -C {} | head -200'   "$@" ;;
-#		export|unset) fzf --preview "eval 'echo \$'{}"         "$@" ;;
-#		ssh)          fzf --preview 'dig {}'                   "$@" ;;
-#		*)            fzf --preview 'bat -n --color=always {}' "$@" ;;
-#		esac
-#}
-#
-##Custom fuzzy completion for "doge" command
-##   e.g. doge **<TAB>
-#_fzf_complete_doge() {
-#	_fzf_complete --multi --reverse --prompt="doge> " -- "$@" < <(
-#			echo very
-#			echo wow
-#			echo such
-#			echo doge
-#			)
-#}
-
-
-## Ctrl-Z: https://sheerun.net/2014/03/21/how-to-boost-your-vim-productivity/
-#fancy-ctrl-z () {
-#if [[ $#BUFFER -eq 0 ]]; then
-#    BUFFER="fg"
-#    zle accept-line
-#else
-#    zle push-input
-#    zle clear-screen
-#fi
-#}
-#zle -N fancy-ctrl-z
-#bindkey '^Z' fancy-ctrl-z
-#
-## Set vim mode
-#bindkey -v
-# alias vim='nvim'
-# alias ls='ls --color'
+curl https://raw.githubusercontent.com/junegunn/fzf/master/shell/completion.zsh > ~/.completion.zsh
+curl http://raw.githubusercontent.com/junegunn/fzf/master/shell/key-bindings.zsh > ~/.key-bindings.zsh
+curl http://raw.githubusercontent.com/ThoDHa/confs/master/zshrc > ~/.zshrc
 
 
 # REGOLITH
