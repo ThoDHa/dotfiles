266efa677f3fb827e942386d1930e69fbc342255
Tho Ha <tho.ha@gatech.edu>
2023-03-14 22:59:06 -0500

    Updating installation again

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+################################################################################
 # set up VIM TMUX and Bash
+################################################################################
 mkdir ~/.vim/tmp/mkdir 
 
-sudo apt-get install tmux vim fd-find bat exuberant-ctags zsh
+sudo apt-get install tmux vim exuberant-ctags
 curl https://raw.githubusercontent.com/ThoDHa/confs/master/vimrc > ~/.vimrc
 curl https://raw.githubusercontent.com/ThoDHa/confs/master/tmux.conf > ~/.tmux.conf
 curl https://raw.githubusercontent.com/ThoDHa/confs/master/ctags > ~/.ctags.d/default.ctags
@@ -15,46 +17,63 @@ git clone https://github.com/tomasiser/vim-code-dark ~/.vim/pack/themes/start/vi
 mkdir -p ~/.vim/pack/git-plugins/start
 git clone --depth 1 https://github.com/dense-analysis/ale.git ~/.vim/pack/git-plugins/start/ale
 
+
+
+################################################################################
 # nvim
+################################################################################
 # Install the latest NVIM using the link.
 # https://github.com/neovim/neovim/releases/tag/stable
+wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb
+sudo apt install ./nvim-linux64.deb
 sudo apt install ripgrep
 python3 -m pip install --user virtualenv
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 nvm install latest
 nvm install-latest-npm
 
 mkdir -m ~/.config/nvim
 git clone https://github.com/ThoDHa/nvim.git ~/.config/nvim
 
-# This sets up bash
-if grep -q "set -o vi" ~/.bashrc; then
-	echo "set -o vi" >> ~/.bashrc
-fi
 
-# Set up fzf to work correctly
-if grep -q "source /usr/share/doc/fzf/examples/key-bindings.bash" ~/.bashrc; then
-	echo "source /usr/share/doc/fzf/examples/key-bindings.bash" >> ~/.bashrc
-fi
 
-if grep -q "source /usr/share/doc/fzf/examples/completion.bash" ~/.bashrc; then
-	echo "source /usr/share/doc/fzf/examples/completion.bash" >> ~/.bashrc
-fi
-
+################################################################################
 # zsh
-# OH MY SH
+################################################################################
+sudo apt-get install zsh
+################################################################################
+# OH MY ZSH
+################################################################################
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 curl http://raw.githubusercontent.com/ThoDHa/confs/master/zshrc > ~/.zshrc
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+
+
+################################################################################
 # FZF
+################################################################################
+sudo apt-get install fd-find bat tree
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 
+
+################################################################################
 # REGOLITH
+################################################################################
+wget -qO - https://regolith-desktop.org/regolith.key | \
+gpg --dearmor | sudo tee /usr/share/keyrings/regolith-archive-keyring.gpg > /dev/null
+
+echo deb "[arch=amd64 signed-by=/usr/share/keyrings/regolith-archive-keyring.gpg] \
+https://regolith-desktop.org/release-ubuntu-jammy-amd64 jammy main" | \
+sudo tee /etc/apt/sources.list.d/regolith.list
+
+sudo apt update
+sudo apt install regolith-desktop regolith-compositor-picom-glx
+sudo apt upgrade
 sudo apt-get install i3xrocks-volume i3xrocks-battery dunst
 sudo apt-get remove i3xrocks-net-traffic cpu-usage regolith-rofication
 
@@ -68,4 +87,3 @@ rm ~/.config/regolith2/i3xrocks/conf.d/40_cpu-usage
 mv ~/.config/regolith2/i3xrocks/conf.d/80_battery ~/.config/regolith2/i3xrocks/conf.d/90_battery
 mv ~/.config/regolith2/i3xrocks/conf.d/80_volume ~/.config/regolith2/i3xrocks/conf.d/70_volume
 mv ~/.config/regolith2/i3xrocks/conf.d/90_time ~/.config/regolith2/i3xrocks/conf.d/80_time
-
