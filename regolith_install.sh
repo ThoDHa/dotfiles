f610cf7fbbaddd2b440fff7fb40a33a6284847cc
Tho Ha <tho.ha@gatech.edu>
2023-12-12 21:38:01 -0600

    Moved regolith to it's own install.

--- regolith_install.sh
+++ regolith_install.sh
@@ -1,28 +1,5 @@
 #!/bin/bash
-
-################################################################################
-# set up VIM TMUX and Bash
-################################################################################
-
-sudo apt-get install git tmux vim exuberant-ctags curl gcc zsh zip unzip python3 python3 wget fd-find bat tree -y
-
-min_setup.sh
-
-mkdir ~/.vim/tmp/mkdir 
-
-
-curl https://raw.githubusercontent.com/ThoDHa/confs/main/vimrc > ~/.vimrc
-curl https://raw.githubusercontent.com/ThoDHa/confs/main/ctags > ~/.ctags.d/default.ctags
-curl https://raw.githubusercontent.com/ThoDHa/confs/main/tmux.conf > ~/.tmux.conf
-
-# This sets up VIM
-mkdir -p ~/.vim/pack/themes/start
-git clone https://github.com/tomasiser/vim-code-dark ~/.vim/pack/themes/start/vim-code-dark 
-
-mkdir -p ~/.vim/pack/git-plugins/start
-git clone --depth 1 https://github.com/dense-analysis/ale.git ~/.vim/pack/git-plugins/start/ale
-
-
+#
 ################################################################################
 # REGOLITH
 ################################################################################
